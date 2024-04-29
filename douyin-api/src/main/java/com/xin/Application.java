b547e57658d31dd7855b2421e47af22caccbb44d
zhangxinxin <1285367184@qq.com>
2024-04-29 15:00:22 +0800

    feat(初始化子工程): 初始化所有子工程并进行依赖管理与创建启动类和暴露api接口

---
+++ douyin-api/src/main/java/com/xin/Application.java
@@ -0,0 +1,12 @@
+package com.xin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// springboot项目启动类
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
